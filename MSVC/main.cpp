90315bf78b8ff5b939bcd50503787e8ae8378132
Andrea Raffin <andrea.raffin.88@gmail.com>
2022-12-05 22:54:54 +0100

    feat: add macro to define input and output messages
    (output messages do not have a callback). Also added test with a small circular buffer library, credit to Embedded Artistry

--- MSVC/main.cpp
+++ MSVC/main.cpp
@@ -5,19 +5,19 @@ extern void example1();
 
 void print_debug(uint8_t* buf, uint16_t len)
 {
-    // Print outbound buffer
-    std::cout << "Message: " << std::endl;
-    for (uint8_t i = 0; i < len; i++)
-    {
-        std::cout << "0x" << std::hex << (int)buf[i];
-        if(i != len-1) std::cout << ",";
-    }
-    std::cout << std::endl;
+	// Print outbound buffer
+	std::cout << "Message: " << std::endl;
+	for (uint8_t i = 0; i < len; i++)
+	{
+		std::cout << "0x" << std::hex << (int)buf[i];
+		if (i != len - 1) std::cout << ",";
+	}
+	std::cout << std::endl;
 }
 
 int main()
 {
-    std::cout << "Hello Neon!\n";
+	std::cout << "Hello Neon!\n";
 
-    example1();
+	example1();
 }
--- neon_uart.h
+++ neon_uart.h
@@ -6,22 +6,26 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-/**=================================================================================================
- **                                 Includes / Defines / Macros
- **===============================================================================================*/
+	/**=================================================================================================
+	 **                                 Includes / Defines / Macros
+	 **===============================================================================================*/
 #include <stdint.h>
 #ifndef ARDUINO
 #include <malloc.h>
 #endif
-/**=================================================================================================
- **                             Public Typedefs / Constants / Macros
- **===============================================================================================*/
+	 /**=================================================================================================
+	  **                             Public Typedefs / Constants / Macros
+	  **===============================================================================================*/
 #define MAGIC1 0xEC
 #define MAGIC2 0x9D
 
-// These macros are a trick for defining and referencing both MSG ID and MSG Struct Typedef
-#define NEON_DEF(NAME,TYPE) NAME ; typedef enum { NAME ##_ID = TYPE } NAME ##_id_t
+	  // These macros are a trick for defining and referencing both MSG ID and MSG Struct Typedef
+#define NEON_DEF(NAME,TYPE) NAME ; typedef enum { NAME ##_ID = TYPE } NAME ##_id_t ; \
+                            void cb_##NAME (void* pv)
+#define NEON_CB(NAME) void cb_##NAME (void* pv)
 #define NEON_MSG(NAME) NAME ##_ID, sizeof( NAME )
+#define NEON_MSG_IN(NAME) NAME ##_ID, sizeof( NAME ), cb_##NAME 
+#define NEON_MSG_OUT(NAME) NAME ##_ID, sizeof( NAME ), 0
 
 // Tweak this to reduce footprint, for small apps you will get away with 16
 #define N_MAX_MSG_TYPES 16
@@ -33,78 +37,78 @@ extern "C" {
 
 // TODO: hash table or linked list to avoid allocating the full array for msg callbacks
 
-  typedef enum {
-    GOT_NONE = 0,
-    GOT_SYNC1 = 1,
-    GOT_SYNC2 = 2,
-    GOT_FAMILY = 3,
-    GOT_ID = 4,
-    GOT_LENGTH1 = 5,
-    GOT_LENGTH2 = 6,
-    GOT_PAYLOAD = 7,
-    GOT_CHKA = 8
-  } parser_state_t;
+	typedef enum {
+		GOT_NONE = 0,
+		GOT_SYNC1 = 1,
+		GOT_SYNC2 = 2,
+		GOT_FAMILY = 3,
+		GOT_ID = 4,
+		GOT_LENGTH1 = 5,
+		GOT_LENGTH2 = 6,
+		GOT_PAYLOAD = 7,
+		GOT_CHKA = 8
+	} parser_state_t;
 
-  typedef enum {
-    NOT_PARSED = 0,
-    PARSED = 1
-  } parser_flag_t;
+	typedef enum {
+		NOT_PARSED = 0,
+		PARSED = 1
+	} parser_flag_t;
 
-  typedef enum {
-    HANDLER_CB_RX = 0,
-    HANDLER_CB_TX = 1
-  } parser_handler_direction_t;
+	typedef enum {
+		HANDLER_CB_RX = 0,
+		HANDLER_CB_TX = 1
+	} parser_handler_direction_t;
 
-  typedef void (*unhandled_callback_t)(uint8_t type);
+	typedef void (*unhandled_callback_t)(uint8_t type);
 
-  typedef struct
-  {
-    /* Message structure: 0xEC 0x9D <family> <type> <lenL> <lenH> <...payload...> <crc1><crc2> */
-    parser_state_t state;
-    uint8_t msg_family;
-    uint8_t msg_id;
-    uint16_t msg_len;
-    uint8_t chka;
-    uint8_t chkb;
-    uint16_t count;
-    uint16_t error_count;
-    uint8_t payload[N_MAX_PAYLOAD];
-    void *cbs;                          // cb register for Handlers and App callbacks, to be cast to parser_callbacks_t*
-    unhandled_callback_t unhandled_cb;  // cb for unhandled function
-  } neon_parser_t;
+	typedef struct
+	{
+		/* Message structure: 0xEC 0x9D <family> <type> <lenL> <lenH> <...payload...> <crc1><crc2> */
+		parser_state_t state;
+		uint8_t msg_family;
+		uint8_t msg_id;
+		uint16_t msg_len;
+		uint8_t chka;
+		uint8_t chkb;
+		uint16_t count;
+		uint16_t error_count;
+		uint8_t payload[N_MAX_PAYLOAD];
+		void* cbs;                          // cb register for Handlers and App callbacks, to be cast to parser_callbacks_t*
+		unhandled_callback_t unhandled_cb;  // cb for unhandled function
+	} neon_parser_t;
 
-  typedef void (*handler_callback_t)(neon_parser_t *pParser, uint8_t txrx, uint8_t type, uint8_t *payload, uint8_t *output);
-  typedef void (*app_callback_t)(void *pv);
+	typedef void (*handler_callback_t)(neon_parser_t* pParser, uint8_t txrx, uint8_t type, uint8_t* payload, uint8_t* output);
+	typedef void (*app_callback_t)(void* pv);
 
-  typedef struct
-  {
-    handler_callback_t h;
-    app_callback_t a;
-    uint16_t msg_size;
-  } parser_callback_t;
+	typedef struct
+	{
+		handler_callback_t h;
+		app_callback_t a;
+		uint16_t msg_size;
+	} parser_callback_t;
 
-  typedef struct
-  {
-    parser_callback_t cb[N_MAX_MSG_TYPES];
-  } parser_callbacks_t;
+	typedef struct
+	{
+		parser_callback_t cb[N_MAX_MSG_TYPES];
+	} parser_callbacks_t;
 
-  /**=================================================================================================
- **                                  Public Functions
- **===============================================================================================*/
-  void neon_parser_init(neon_parser_t *pParser);
-  void neon_parser_deinit(neon_parser_t *pParser);
-  void neon_parser_reset(neon_parser_t *pParser);
+	/**=================================================================================================
+   **                                  Public Functions
+   **===============================================================================================*/
+	void neon_parser_init(neon_parser_t* pParser);
+	void neon_parser_deinit(neon_parser_t* pParser);
+	void neon_parser_reset(neon_parser_t* pParser);
 
-  // Application Callbacks
-  void neon_parser_set_cb(neon_parser_t *pParser, uint8_t msg_type, app_callback_t cb);
-  void neon_parser_set_unhandled_cb(neon_parser_t *pParser, unhandled_callback_t cb);
+	// Application Callbacks
+	void neon_parser_set_cb(neon_parser_t* pParser, uint8_t msg_type, app_callback_t cb);
+	void neon_parser_set_unhandled_cb(neon_parser_t* pParser, unhandled_callback_t cb);
 
-  // TX
-  void neon_define_message(neon_parser_t *pParser, uint8_t msg_type, uint16_t length, app_callback_t cb);
-  uint8_t neon_build_message(neon_parser_t *pParser, uint8_t *pay, uint8_t type, uint16_t paylen, uint8_t *msg_out);
+	// TX
+	void neon_define_message(neon_parser_t* pParser, uint8_t msg_type, uint16_t length, app_callback_t cb);
+	uint8_t neon_build_message(neon_parser_t* pParser, uint8_t* pay, uint8_t type, uint16_t paylen, uint8_t* msg_out);
 
-  // RX
-  parser_flag_t neon_parse_char(neon_parser_t *pParser, uint8_t b);
+	// RX
+	parser_flag_t neon_parse_char(neon_parser_t* pParser, uint8_t b);
 
 #ifdef __cplusplus
 }  // extern "C"
